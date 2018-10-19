1aeb068ca9bea692599f91076e0cbb38eca07649
gammyp <gamebep@gmail.com>
2018-10-19 09:18:18 +0700

    [INIT] Create springboot project.

---
+++ src/main/java/cf/alpaca202/alpacacommerce/AlpacacommerceApplication.java
@@ -0,0 +1,12 @@
+package cf.alpaca202.alpacacommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlpacacommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlpacacommerceApplication.class, args);
+	}
+}
